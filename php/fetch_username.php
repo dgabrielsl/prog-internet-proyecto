a315c7cdb262b07311a2804b98ad39649bc60f9c
Ing. Gabriel Solano <dgabrielsolo@gmail.com>
2024-08-22 12:37:26 -0600

    .

--- php/fetch_username.php
+++ php/fetch_username.php
@@ -4,25 +4,19 @@ $clave = "";
 $bbdd = "vip_tickets_solutions";
 $equipo = "localhost";
 
-// Conectar a la base de datos
 $conn = mysqli_connect($equipo, $usuario, $clave, $bbdd);
 
-// Consulta para obtener el valor de act_user de la tabla 'session'
 $query = "SELECT act_user FROM session LIMIT 1";
 $result = mysqli_query($conn, $query);
 
-// Preparar datos para JSON
 $data = array('username' => 'No user found');
 
-// Comprobar si se obtuvo un resultado
-if ($row = mysqli_fetch_assoc($result)) {
+if($row = mysqli_fetch_assoc($result)) {
     $data['username'] = $row['act_user'];
-}
+};
 
-// Devolver el resultado en formato JSON
 header('Content-Type: application/json');
 echo json_encode($data);
 
-// Cerrar la conexión
 mysqli_close($conn);
 ?>
---
+++ php/update-inventory.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "vip_tickets_solutions";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+    die("Conexión fallida: " . $conn->connect_error);
+};
+
+$act_name = $_POST['act-name'];
+$act_brand = $_POST['act-brand'];
+$act_model = $_POST['act-model'];
+$act_serial = $_POST['act-serial'];
+
+$inserted_by = 
+$log_date = date('Y-m-d');
+$time_mark = date('H:i:s');
+
+$insertQuery = "INSERT INTO inventory (act_name, act_brand, act_model, act_serial, inserted_by, log_date, time_mark) VALUES ('$act_name', '$act_brand', '$act_model', '$act_serial', '$inserted_by', '$log_date', '$time_mark')";
+
+if($conn -> query($insertQuery) === TRUE){
+    header("Location: ../html/inventory.html?act_name=" . $act_name . "&act_brand=" . $act_brand . "&act_model=" . $act_model . "&act_serial=" . $act_serial);
+}else{
+    header("Location: ../html/inventory.html?error=1");
+};
+
+$conn->close();
+?>
